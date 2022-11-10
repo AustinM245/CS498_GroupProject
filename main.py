57ac600523faa6390b456e68ecede90329a683b1
AustinM245 <83526102+AustinM245@users.noreply.github.com>
2022-11-09 21:40:42 -0500

    The data is now fully parsed with readAllFiles

--- main.py
+++ main.py
@@ -30,32 +30,32 @@ def readAllFiles(folderName):
         #Use ord() to increment the letter for the filepath
         letter = chr(ord('A') + x)
         filePath = folderName + '\\' + letter +'\\'
-        readFromJSON(filePath)
+        data = readFromJSON(filePath)
+        #keyPointList = parseData(data)
+        print(data)
+        
   
 #Reads the string from the JSON file and returns it
 def readFromJSON(filePath):
     #Get current working directory, add it to file path
     filePath = os.getcwd() + '\\' + filePath 
-    print(filePath)
+    #print(filePath)
     #Get the number of files in the folder so we know how many we need to read
-    print(len(os.listdir(filePath)))
-    
-    #Each filename for the JSONS is formatted as follows:
-    # 'xxxxxxxxxxxx_keypoints.json'
-    #Where the sequence of x's are integers that represent the frame number
-    #that the file corresponds to. The input variable x will be the frame number,
-    #but it needs to be 12 characters long, with remaining characters being
-    #leading zeros. We can use .zfill(12) to add leading zeros that stop at 12
-    #total characters
-    #x = x.zfill(12)
+    numFiles = len(os.listdir(filePath))
     
     #Can concatenate the file name! Since the long series of 0's increments to
     #represent each file, we can also increment it in a for loop when reading
     #in multiple files
-    #f = open(filePath + '\\' + 'VR1_' + x + '_keypoints.json')
-    #data = json.load(f)
-    #f.close()
-    #return data
+    data = []
+    # Get the file pathnames in each folder and get ALL data from every JSON
+    # file. Stuff it all into one big list data[] to be processed later
+    for file in os.listdir(filePath):
+        f = open(filePath + file)
+        temp = json.load(f)
+        f.close()
+        temp = parseData(temp)
+        data.append(temp)
+    return data
 
 def parseData(data):
     
@@ -68,7 +68,7 @@ def parseData(data):
     # keypoint mappings. For now, we're just using the pose_keypoints_2d
     # mapping, so these for loops go into the nested lists to retrieve it
     for people in data['people']:
-        for getKeypoints in people['pose_keypoints_2d']:
+        for getKeypoints in people['hand_right_keypoints_2d']:
             keypoints.append(getKeypoints)
             
     #print(keypoints)
