ac60b4c3924bf42274b2e6e6562ea50f60961a43
AustinM245 <83526102+AustinM245@users.noreply.github.com>
2022-11-09 22:17:56 -0500

    Edits readFromJSON to create yList and completes readAllFiles to give full list of data and labels

--- main.py
+++ main.py
@@ -30,21 +30,25 @@ def readAllFiles(folderName):
         #Use ord() to increment the letter for the filepath
         letter = chr(ord('A') + x)
         filePath = folderName + '\\' + letter +'\\'
-        data = readFromJSON(filePath)
-        print(data)
+        allData = readFromJSON(filePath, x)
+        keypointData.append(allData)
+    print(keypointData)
   
 #Reads the string from the JSON file and returns it
-def readFromJSON(filePath):
+def readFromJSON(filePath, yLabel):
     #Get current working directory, add it to file path
     filePath = os.getcwd() + '\\' + filePath 
     #print(filePath)
     #Get the number of files in the folder so we know how many we need to read
     numFiles = len(os.listdir(filePath))
+    #Create a list of y labels that has size of the number of files in the folder
+    yData = [yLabel] * numFiles
+    #print(yData)
     #Can concatenate the file name! Since the long series of 0's increments to
     #represent each file, we can also increment it in a for loop when reading
     #in multiple files
     
-    data = []
+    xData = []
     # Get the file pathnames in each folder and get ALL data from every JSON
     # file. Stuff it all into one big list data[] to be processed later
     for file in os.listdir(filePath):
@@ -52,8 +56,9 @@ def readFromJSON(filePath):
         temp = json.load(f)
         f.close()
         temp = parseData(temp)
-        data.append(temp)
-    return data
+        xData.append(temp)
+    allData = [xData, yData]
+    return allData
 
 def parseData(data):
     
@@ -68,17 +73,12 @@ def parseData(data):
     for people in data['people']:
         for getKeypoints in people['hand_right_keypoints_2d']:
             keypoints.append(getKeypoints)
-            
-    #print(keypoints)
-    #print('\n')
     
     #After getting the keypoints, they are all in one big list. This command
     # parses the big list into 25 lists each with 3 elements in the following
     # form (as mentioned above)
     #[x coordinate, y coordinate, confidence level]
     keypointsList = [keypoints[x:x+3] for x in range(0, len(keypoints), 3)]
-    
-    print(keypointsList)
     
     return keypointsList
 
